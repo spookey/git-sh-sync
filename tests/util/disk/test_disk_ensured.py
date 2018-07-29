6dd4f27690e72944e05d5796c0a6a92855925bea
spky <frieder.griesshammer@der-beweis.de>
2018-07-29 11:17:44 +0200

    Fix tests on travis as python3.5 does not auto convert py.path.local to strings

--- tests/util/disk/test_disk_ensured.py
+++ tests/util/disk/test_disk_ensured.py
@@ -7,7 +7,7 @@ def test_ensured_folder(tmpdir):
     folder = tmpdir.join('folder')
     assert folder.stat(raising=False) is None
 
-    assert ensured(folder, folder=True) == folder
+    assert ensured(str(folder), folder=True) == folder
 
     assert folder.stat(raising=False) is not None
     assert tmpdir.remove(folder) is None
@@ -19,7 +19,7 @@ def test_ensured_file(tmpdir):
     assert folder.stat(raising=False) is None
     assert file.stat(raising=False) is None
 
-    assert ensured(file, folder=False) == file
+    assert ensured(str(file), folder=False) == file
 
     assert folder.stat(raising=False) is not None
     assert file.stat(raising=False) is None
@@ -31,7 +31,7 @@ def test_ensured_logging(caplog, tmpdir):
     folder = tmpdir.join('folder')
     file = folder.join('file')
 
-    assert ensured(file, folder=False) == file
+    assert ensured(str(file), folder=False) == file
 
     assert tmpdir.remove(folder) is None
 
